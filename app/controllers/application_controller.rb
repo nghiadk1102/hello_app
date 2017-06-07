d850acecf9eb403da337f2cd91d8b4acd0160a05
nghiadk <nghiadk1102@gmail.com>
2017-06-07 14:44:01 +0700

    Inittialize reposiory

---
+++ app/controllers/application_controller.rb
@@ -0,0 +1,10 @@
+class ApplicationController < ActionController::Base
+  protect_from_forgery with: :exception
+
+  def hello
+    render html: "¡Hola, mundo!"
+  end
+  def byebye
+    render html: "byebye worlds ?"
+  end
+end
